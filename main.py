8c6151dbfd42c40a4e1d9a56cf032c725b211bba
samfallahi <mfallahi@uncc.edu>
2022-08-07 16:03:57 -0400

    feat: add logger

--- main.py
+++ main.py
@@ -1,25 +1,32 @@
 from configs import config as cfg
-from utils.config import Config
+from utils.config import Config, Log
 from dataloader.databuilder import DataBuilder
+from dataloader.dataloader import DataLoader
+from dataloader.datareader import DataReader
 from executor.executor import Executor
 from datetime import datetime
-import json
-import os.path
+
 
 def main():
+    logger = Log()
     conf = Config.from_json(cfg.CFG)
-    path = conf.data.path + "test" #3p6
 
-    data = DataBuilder(path)
+    data_reader = DataReader(conf)
 
-    train_loader = data.train_data(batch_size=conf.train.batch_size)
-    test_loader = data.test_data(batch_size=conf.train.batch_size)
+    data = data_reader.load_and_standardize_data()
+
+    train_data_set = DataBuilder(data=data, train=True)
+    test_data_set = DataBuilder(data=data, train=False)
+
+    train_loader = DataLoader(databuilder=train_data_set, batch_size=conf.train.batch_size).loader()
+    test_loader = DataLoader(databuilder=test_data_set, batch_size=conf.train.batch_size).loader()
 
     input_feature = train_loader.dataset.x.shape[1]
 
+    print(f"Start training: {datetime.now()}")
     exe = Executor(cfg=conf.train, input_feature=input_feature)
-    train_losses=[]
-    test_losses=[]
+    train_losses = []
+    test_losses = []
 
     for epoch in range(1, conf.train.epochs + 1):
         train = exe.train(epoch, train_loader)
@@ -27,79 +34,14 @@ def main():
         test = exe.test(epoch, test_loader)
         test_losses.append(test)
 
-    with open(os.path.join("logs", f"{datetime.now()}-train.json"), "w") as f:
-        json.dump(train_losses, f, indent=4)
+    print(f"Finish training: {datetime.now()}")
 
-    with open(os.path.join("logs", f"{datetime.now()}-test.json"), "w") as f:
-        json.dump(train_losses, f, indent=4)
+    logger.write_file(data=train_losses, file_name="train")
+    logger.write_file(data=test_losses, file_name="test")
+
+    print(f"Saving model: {datetime.now()}")
+    exe.save_model()
 
 
 if __name__ == "__main__":
     main()
-
-    # H = 50
-    # H2 = 12
-    # device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    # model = autoencoder.Autoencoder(D_in, H, H2).to(device)
-    # custom_loss = autoencoder.CustomLoss()
-# conf = Config.from_json(cfg.CFG)
-# path = conf.data.path + "test"
-#
-# data = DataBuilder(path)
-#
-# trainloader = data.train_data(batch_size=conf.train.batch_size)
-# testloader = data.test_data(batch_size=conf.train.batch_size)
-
-# print(type(a.train_data(batch_size=conf.train.batch_size).dataset.x))
-# print(a.test_data(batch_size=conf.train.batch_size).dataset.len)
-# print(a.test_data(batch_size=conf.train.batch_size).dataset.x)
-
-# device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-
-# D_in = trainloader.dataset.x.shape[1]
-#
-# H = 50
-# H2 = 12
-# model = autoencoder.Autoencoder(D_in, H, H2).to(device)
-# optimizer = optim.Adam(model.parameters(), lr=1e-3)
-# loss_mse = autoencoder.CustomLoss()
-
-
-# epochs = 1500
-# log_interval = 50
-# val_losses = []
-# train_losses = []
-# test_losses = []
-
-
-# def train(epoch):
-#     model.train()
-#     train_loss = 0
-#     for batch_idx, data in enumerate(trainloader):
-#         data = data.to(device)
-#         optimizer.zero_grad()
-#         recon_batch, mu, logvar = model(data) # forward
-#         loss = loss_mse(recon_batch, data, mu, logvar) # calling custom loss
-#         loss.backward()
-#         train_loss += loss.item()
-#         optimizer.step()
-#     if epoch % 200 == 0:
-#         print('====> Epoch: {} Average training loss: {:.4f}'.format(
-#             epoch, train_loss / len(trainloader.dataset)))
-#         train_losses.append(train_loss / len(trainloader.dataset))
-
-
-# def test(epoch):
-#     with torch.no_grad():
-#         test_loss = 0
-#         for batch_idx, data in enumerate(testloader):
-#             data = data.to(device)
-#             optimizer.zero_grad()
-#             recon_batch, mu, logvar = model(data)
-#             loss = loss_mse(recon_batch, data, mu, logvar)
-#             test_loss += loss.item()
-#             if epoch % 200 == 0:
-#                 print('====> Epoch: {} Average test loss: {:.4f}'.format(
-#                     epoch, test_loss / len(testloader.dataset)))
-#             test_losses.append(test_loss / len(testloader.dataset))
