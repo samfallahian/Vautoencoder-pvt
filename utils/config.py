58f73144de89f23f1b782d4cba456a1e8dbb5594
samfallahi <mfallahi@uncc.edu>
2022-08-07 16:02:50 -0400

    feat: add log writer

--- utils/config.py
+++ utils/config.py
@@ -1,9 +1,9 @@
 import json
+from datetime import datetime
+import os.path
 
 
 class Config:
-    """Config class which contains data, train and model hyperparameters"""
-
     def __init__(self, data, train, model):
         self.data = data
         self.train = train
@@ -15,7 +15,18 @@ class Config:
         params = json.loads(json.dumps(cfg), object_hook=HelperObject)
         return cls(params.data, params.train, params.model)
 
+
 class HelperObject(object):
     """Helper class to convert json into Python object"""
+
     def __init__(self, dict_):
         self.__dict__.update(dict_)
+
+
+class Log:
+    def write_file(self, data, file_name):
+        with open(os.path.join("logs", f"{datetime.now()}-{file_name}.json"), "w") as f:
+            json.dump(data, f, indent=4)
+
+    def append_file(self, data, file_name):
+        pass
