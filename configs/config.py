3a12db0558ec61f1b5eba4eb4d29993b16f4b42c
samfallahi <mfallahi@uncc.edu>
2022-08-07 16:04:32 -0400

    fix: structure improvement

--- configs/config.py
+++ configs/config.py
@@ -2,13 +2,15 @@ CFG = {
     "data": {
         "path": "/mnt/d/sources/data/DL-PTV/merged/",
         "speeds": [0.06528, 0.06852, 0.07824, 0.09768, 0.10092, 0.11064, 0.12036, 0.13008, 0.16248, 0.17868],
-        "folders": ["test1", "test2", "test3"]
+        "folders": ["3p6", "4p4", "4p6","5p2","6p4","6p6","7p2","7p8","8p4","10p4","11p4"],
+        "folder": "3p6",
+        "frame": 51
     },
     "train": {
-        "learning_rate": 1e-3,
-        "batch_size": 1024,
+        "learning_rate": 1e-2,
+        "batch_size": 512,
         "buffer_size": 1000,
-        "epochs": 1000,
+        "epochs": 500,
         "H": 50,
         "H2": 12,
         "optimizer": {
